ee63adc3ef9f561a3c60fb6694f807d9cc1d3149
MMWVM <158175163+MMWVM@users.noreply.github.com>
2024-01-30 15:46:49 +0200

    Add files via upload

---
+++ mody.py
@@ -0,0 +1,19 @@
+import os
+
+
+class Mody(object):
+    ELDYLAR = os.environ.get("ELDYLAR", "")
+    
+    OWNER = os.environ.get("OWNER", "1591029063")
+    
+    ID_BOT = int(os.environ.get("ID_BOT", ""))
+    
+    USER_BOT = os.environ.get("USER_BOT", "")
+    
+    MAX_ACCOUNTS = int(os.environ.get("MAX_ACCOUNTS", ""))
+    
+    API_ID = os.environ.get("API_ID", "25281175")
+    
+    API_HASH = os.environ.get("API_HASH", "6d99cb2b60a2c519fc1f99bd19565730")
+    
+
